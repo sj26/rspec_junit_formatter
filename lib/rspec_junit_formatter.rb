e42c5e51e0676ff7a2c967b61db26512afa9ea22
Samuel Cochran <sj26@sj26.com>
2012-12-06 11:13:36 +0800

    Revert "Cope with format_backtrace change in rspec-core 2.12"
    This reverts commit 9d0053bcab91ecddc8bb9471cd7d686bc5c54b07.
    
    This API change was reverted in rspec 2.12.1:
    
    https://github.com/rspec/rspec-core/commit/39857f30ebc8f19374ece61e3c59cd12b5986f7d

--- lib/rspec_junit_formatter.rb
+++ lib/rspec_junit_formatter.rb
@@ -1,4 +1,8 @@
-require "rspec/core/formatters/j_unit_formatter"
+require 'builder'
+require 'rspec'
+
+require 'rspec/core/formatters/base_formatter'
+require 'rspec/core/formatters/j_unit_formatter'
 
 # Make it easier to use
 RspecJunitFormatter = RSpecJUnitFormatter = RSpec::Core::Formatters::JUnitFormatter

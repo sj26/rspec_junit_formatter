1b7d6ddb9fec5cc68e5130705f151615a062abc0
Samuel Cochran <sj26@sj26.com>
2014-06-04 21:39:05 +1000

    Clearly separate public and private interface

--- lib/rspec_junit_formatter.rb
+++ lib/rspec_junit_formatter.rb
@@ -8,53 +8,60 @@ require "rspec/core/formatters/base_formatter"
 # Dumps rspec results as a JUnit XML file.
 # Based on XML schema: http://windyroad.org/dl/Open%20Source/JUnit.xsd
 class RSpecJUnitFormatter < RSpec::Core::Formatters::BaseFormatter
+  attr_reader :started
+
+  def start(example_count)
+    @started = Time.now
+    super
+  end
+
+  def dump_summary(duration, example_count, failure_count, pending_count)
+    super
+    xml_dump
+  end
+
+private
+
   def xml
     @xml ||= Builder::XmlMarkup.new :target => output, :indent => 2
   end
 
-  def start example_count
-    @start = Time.now
-    super
-  end
-
-  def dump_summary duration, example_count, failure_count, pending_count
-    super
-
+  def xml_dump
     xml.instruct!
-    xml.testsuite :name => "rspec", :tests => example_count, :failures => failure_count, :errors => 0, :time => "%.6f" % duration, :timestamp => @start.iso8601 do
+    xml.testsuite :name => "rspec", :tests => example_count, :failures => failure_count, :errors => 0, :time => "%.6f" % duration, :timestamp => started.iso8601 do
       xml.properties
       examples.each do |example|
-        send :"dump_summary_example_#{example.execution_result[:status]}", example
+        send :"xml_dump_#{example.execution_result[:status]}", example
       end
     end
   end
 
-  def xml_example example, &block
-    xml.testcase :classname => example_classname(example), :name => example.full_description, :time => "%.6f" % example.execution_result[:run_time], &block
+  def xml_dump_passed(example)
+    xml_dump_example example
   end
 
-  def dump_summary_example_passed example
-    xml_example example
-  end
-
-  def dump_summary_example_pending example
-    xml_example example do
+  def xml_dump_pending(example)
+    xml_dump_example example do
       xml.skipped
     end
   end
 
-  def dump_summary_example_failed example
+  def xml_dump_failed(example)
     exception = example.execution_result[:exception]
     backtrace = format_backtrace exception.backtrace, example
 
-    xml_example example do
+    xml_dump_example example do
       xml.failure :message => exception.to_s, :type => exception.class.name do
         xml.cdata! "#{exception.message}\n#{backtrace.join "\n"}"
       end
     end
   end
 
-  def example_classname example
+  def xml_dump_example(example, &block)
+    xml.testcase :classname => example_classname(example), :name => example.full_description, :time => "%.6f" % example.execution_result[:run_time], &block
+  end
+
+  def example_classname(example)
     example.file_path.sub(%r{\.[^/]*\Z}, "").gsub("/", ".").gsub(%r{\A\.+|\.+\Z}, "")
   end
 end
